1bd64146c3403a8ca8cd0b153700558c987a06d4
arthuraleqsan <aleqsanyanarthur@gmail.com>
2023-08-23 14:55:02 +0400

    v0.1.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+const path = require('path');
+const srcPath = path.join(__dirname, 'src');
+
+console.log(123, __dirname);
 
 module.exports = {
-    entry: './src/index.tsx', // Your library's entry file
+    entry: {
+        app: path.join(srcPath, 'index.tsx'), // Your library's entry file
+    },
     output: {
-        // path: './public/index.html',
+        path: path.resolve(__dirname, 'public'),
         filename: 'index.tsx',
         library: 'react-messaging-library', // The global variable name when used as a library
-        libraryTarget: 'umd',
     },
 };
